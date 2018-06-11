17c6edf5b7bff9a1ba9a35c0037d3f5703be3371
Samu András <samu.andris1@gmail.com>
2018-06-11 15:44:15 +0200

    init repo

---
+++ Brainooo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brainooo
+//
+//  Created by Samu András on 2018. 06. 11..
+//  Copyright © 2018. Samu András. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
